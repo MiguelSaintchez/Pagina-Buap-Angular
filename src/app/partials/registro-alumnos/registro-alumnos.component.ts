de23c7f26b6236acadca7f1165f80d8b91b7c745
Miguel <msaintchez@gmail.com>
2024-11-14 00:24:01 -0600

    Update registro-alumnos.component.ts

--- src/app/partials/registro-alumnos/registro-alumnos.component.ts
+++ src/app/partials/registro-alumnos/registro-alumnos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-registro-alumnos',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./registro-alumnos.component.scss']
 })
 export class RegistroAlumnosComponent {
+  @Input() datos_user: string = ""
+  @Input() rol:string = ""
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
