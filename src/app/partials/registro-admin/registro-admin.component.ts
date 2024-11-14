a47595f6d7a7dca3686dee1104f9f34cef2a57a8
Miguel <msaintchez@gmail.com>
2024-11-14 01:29:21 -0600

    Campos de formulario de administrador definidos
    -Se agregaron los campos de formulario de administrador.

--- src/app/partials/registro-admin/registro-admin.component.ts
+++ src/app/partials/registro-admin/registro-admin.component.ts
@@ -9,8 +9,45 @@ export class RegistroAdminComponent implements OnInit {
   @Input() datos_user: string = ""
   @Input() rol:string = ""
 
+//Para contraseñas
+  public hide_1: boolean = false;
+  public hide_2: boolean = false;
+  public inputType_1: string = 'password';
+  public inputType_2: string = 'password';
+
+  public admin:any= {};
+  public token: string = "";
+  public errors:any={};
+  public editar:boolean = false;
+  public idUser: Number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+    //Funciones para password
+  showPassword()
+  {
+    if(this.inputType_1 == 'password'){
+      this.inputType_1 = 'text';
+      this.hide_1 = true;
+    }
+    else{
+      this.inputType_1 = 'password';
+      this.hide_1 = false;
+    }
+  }
+
+  showPwdConfirmar()
+  {
+    if(this.inputType_2 == 'password'){
+      this.inputType_2 = 'text';
+      this.hide_2 = true;
+    }
+    else{
+      this.inputType_2 = 'password';
+      this.hide_2 = false;
+    }
   }
 }
