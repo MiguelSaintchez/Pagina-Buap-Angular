99a3ebb424f2b016b8ace0b3649fc20e1cf65a6f
Miguel <msaintchez@gmail.com>
2024-11-13 19:17:04 -0600

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginScreenComponent } from './screens/login-screen/login-screen.component';
+import { RegistroUsuariosScreenComponent } from './screens/registro-usuarios-screen/registro-usuarios-screen.component';
+//Elementos de angular material
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatInputModule} from '@angular/material/input';
+import { NavbarComponent } from './partials/navbar/navbar.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginScreenComponent,
+    RegistroUsuariosScreenComponent,
+    NavbarComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    FormsModule,
+    MatInputModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/partials/navbar/navbar.component.ts
@@ -0,0 +1,69 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+//import { FacadeService } from 'src/app/services/facade.service';
+declare var $:any;
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit{
+  @Input() tipo: string = "";
+  @Input() rol:string ="";
+
+  public token : string = "";
+  public editar:boolean = false;
+
+  constructor(
+    //private facadeService: FacadeService,
+    public activatedRoute: ActivatedRoute,
+    private router: Router
+  ){}
+
+  ngOnInit() {
+    //this.rol = this.facadeService.getUserGroup();
+    console.log("Rol user: ", this.rol);
+    //Validar que haya inicio de sesión
+    //Obtengo el token del login
+    //this.token = this.facadeService.getSessionToken();
+    //El primer if valida si existe un parámetro en la URL
+    if(this.activatedRoute.snapshot.params['id'] != undefined){
+      this.editar = true;
+    }
+
+  }
+
+  public goRegistro(){
+    this.router.navigate(["registro-usuarios"]);
+  }
+  //Cerrar sesión
+
+
+  public clickNavLink(link: string){
+    this.router.navigate([link]);
+    setTimeout(() => {
+      this.activarLink(link);
+    }, 100);
+  }
+  public activarLink(link: string){
+    if(link == "alumnos"){
+      $("#principal").removeClass("active");
+      $("#maestro").removeClass("active");
+      $("#alumno").addClass("active");
+    }else if(link == "maestros"){
+      $("#principal").removeClass("active");
+      $("#alumno").removeClass("active");
+      $("#maestro").addClass("active");
+    }else if(link == "home"){
+      $("#alumno").removeClass("active");
+      $("#maestro").removeClass("active");
+      $("#principal").addClass("active");
+    }else if(link == "graficas"){
+      $("#alumno").removeClass("active");
+      $("#maestro").removeClass("active");
+      $("#principal").removeClass("active");
+      $("#graficas").addClass("active");
+    }
+  }
+}
